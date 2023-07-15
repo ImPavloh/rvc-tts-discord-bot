f780ba310cd65509c067c7e09123583e0540e20a
Pavloh <impavloh@gmail.com>
2023-07-15 14:16:47 +0200

    Update onnx_inference.py

--- lib/infer_pack/onnx_inference.py
+++ lib/infer_pack/onnx_inference.py
@@ -16,11 +16,9 @@ class ContentVectorModel:
         providers = ["CPUExecutionProvider"]
         self.model = onnxruntime.InferenceSession(vector_path, providers=providers)
 
-    def __call__(self, audio_wave):
-        return self.process_audio(audio_wave)
+    def __call__(self, audio_wave): return self.process_audio(audio_wave)
 
-    @staticmethod
-    def process_audio(audio_wave):
+    def process_audio(self, audio_wave):
         features = audio_wave.mean(-1) if audio_wave.ndim == 2 else audio_wave
         features = np.expand_dims(np.expand_dims(features, 0), 0)
         onnx_input = {self.model.get_inputs()[0].name: features}
@@ -37,24 +35,16 @@ class OnnxRVC:
         self.hop_size = hop_size
 
     def forward(self, hubert, hubert_length, pitch, pitchf, ds, rnd):
-        onnx_input = {
-            self.model.get_inputs()[0].name: hubert,
-            self.model.get_inputs()[1].name: hubert_length,
-            self.model.get_inputs()[2].name: pitch,
-            self.model.get_inputs()[3].name: pitchf,
-            self.model.get_inputs()[4].name: ds,
-            self.model.get_inputs()[5].name: rnd,
-        }
+        onnx_input = {self.model.get_inputs()[0].name: hubert, self.model.get_inputs()[1].name: hubert_length, self.model.get_inputs()[2].name: pitch, self.model.get_inputs()[3].name: pitchf, self.model.get_inputs()[4].name: ds, self.model.get_inputs()[5].name: rnd}
         return (self.model.run(None, onnx_input)[0] * 32767).astype(np.int16)
 
     def inference(self, raw_path, sid, f0_method="pm", f0_up_key=0, pad_time=0.5, cr_threshold=0.02):
         f0_predictor = PMF0Predictor(hop_length=self.hop_size, sampling_rate=self.sampling_rate, threshold=cr_threshold)
         wav, sr = librosa.load(raw_path, sr=self.sampling_rate)
         org_length = len(wav)
-        if org_length / sr > MAX_LENGTH:
-            raise RuntimeError("Reached Max Length")
+        if org_length / sr > MAX_LENGTH: raise RuntimeError("Reached Max Length")
 
-        wav16k = librosa.resample(wav, orig_sr=self.sampling_rate, target_sr=RESAMPLING_RATE)
+        wav16k = librosa.resample(wav, orig_sr=sr, target_sr=RESAMPLING_RATE)
         hubert = self.vec_model(wav16k)
         hubert = np.repeat(hubert, 2, axis=2).transpose(0, 2, 1).astype(np.float32)
         hubert_length = hubert.shape[1]
@@ -74,4 +64,4 @@ class OnnxRVC:
 
         out_wav = self.forward(hubert, hubert_length, pitch, pitchf, ds, rnd).squeeze()
         out_wav = np.pad(out_wav, (0, 2 * self.hop_size), "constant")
-        return out_wav[0:org_length]
+        return out_wav[:org_length]
